ae39872b9b1d691b6ddf608f92a82ff8d21340b6
RichardUG <rsug.rsug@gmail.com>
2023-12-08 19:29:48 -0500

    change url api

--- src/Component/generator.jsx
+++ src/Component/generator.jsx
@@ -316,7 +316,7 @@ const DynamicTable = () => {
 
   const handleSendTemplate = () => {
     const arrayJson = cleanedTemplateData;
-    fetch('http://ec2-18-206-45-139.compute-1.amazonaws.com:7000/v1/synth/deployments', {
+    fetch('http://ec2-3-235-234-225.compute-1.amazonaws.com:7000/v1/synth/deployments', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
